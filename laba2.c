204bc7de99689007e0c0952168d73a9316accf1f
FatkhutdinovVD <fatkhutdinov@edu.surgu.ru>
2021-04-13 14:09:16 +0500

    лаба2 загружена

--- laba2.c
+++ laba2.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-int main ()
+int main()
 {
-    double x, a;
-    printf("Enter x ->");
-    scanf("%f",&x);
-    printf("Enter a ->");
-    scanf("%f",&a);
-    double y = (1/2)*(pow(a,x)+pow(a,-x));
-    double z = sqrt (2+y-pow(y,2));
+		const double a = 2.0;
+		double x, z, y;
+		printf(" Enter x {-1.89997 < x < 1.89997} -> ");
+	  scanf("%lf", &x);
+		if (x < 1.89997 && x > -1.89997)
+		{
+			  y = 0.5 * (pow(a, x) + pow(a, -x));
+	  		z = sqrt(2.0 + y - y * y);
+				printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+		}
+		else
+	  {
+				printf("x value is incorrect !\n");
+		}
+	  return 0;
+}
