b76ae3adb82524438dae968549046e8688d0b067
GafurovDD <81227940+GafurovDD@users.noreply.github.com>
2021-05-26 19:52:13 +0500

    Add files via upload

---
+++ laba7.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void fill(int I, int J, int a[][J])
+{
+    int i, j;
+    for (i = 0; i < I; i++)
+    {
+        for (j = 0; j < J; j++)
+            a[i][j] = rand() % 10;
+    }
+}
+void printmas(int I, int J, int a[][J])
+{
+    int i, j;
+    for (i = 0; i < I; i++)
+    {
+        for (j = 0; j < J; j++)
+            printf("%6d", a[i][j]);
+        printf("\n");
+    }
+}
+void sum(int I, int J, int massum[][J], int mas1[][J], int mas2[][J])
+{
+	int i, j;
+	printf ("Сумма\n");
+    for (i = 0; i < I; i++)
+    {
+        for (j = 0; j < J; j++)
+		{
+			massum[i][j] = mas1[i][j] + mas2[i][j];
+			printf("%6d", massum[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	srand(time(NULL));
+	int I, J;
+	printf("Введите количество строчек двухмерных массивов - > ");
+	scanf("%d", &I);
+	printf("Введите количество столбцов двухмерных массивов - > ");
+	scanf("%d", &J);
+	int mas1[I][J], mas2[I][J], massum[I][J];
+	fill(I, J, mas1);
+	printf("Массив 1 \n");
+    printmas(I, J, mas1);
+	fill(I, J, mas2);
+	printf("Массив 2 \n");
+    printmas(I, J, mas2);
+	sum(I, J, massum, mas1, mas2);
+	return 0;
+}
